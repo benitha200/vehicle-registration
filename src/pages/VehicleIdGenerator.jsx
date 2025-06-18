52607e0a10469b68af30ac7445ab2fa5fdaa32fa
benitha200 <benithaiyuyisenga2002@gmail.com>
2025-06-18 21:22:39 +0200

    update vehicle Id generator

--- src/pages/VehicleIdGenerator.jsx
+++ src/pages/VehicleIdGenerator.jsx
@@ -121,28 +121,28 @@ const VehicleIdGenerator = () => {
                         </div>
                         <div className="grid grid-cols-3 gap-4 mb-4">
                             {/* Left column - Owner info */}
-                            <div className="col-span-2 space-y-1 p-4">
-                                <div className="flex">
+                            <div className="col-span-2 space-y-1 gap-2 p-4">
+                                <div className="flex items-start gap-2">
                                     <span className="text-sm text-black-900 uppercase">Noms (ou Rais. Soc.):</span>
                                     <span className="text-sm font-semibold text-black">{formData.proprietaire || 'Mr BAGAYAMUKWE TADJI GUSTAVE'}</span>
                                 </div>
-                                <div className="flex">
+                                <div className="flex items-start gap-2">
                                     <span className="text-sm text-black-900 uppercase">Adresse Phys:</span>
                                     <span className="text-sm font-semibold text-black">{formData.adresse || 'DU PLATEAU Q/Nyalukemba CIBANDA BUKAVU'}</span>
                                 </div>
-                                <div className="flex">
+                                <div className="flex items-start gap-2">
                                     <span className="text-sm  text-black-900 uppercase">N° Impôt:</span>
                                     <span className="text-sm font-semibold text-black">{formData.numeroImmatriculation || 'A2036989D'}</span>
                                 </div>
-                                <div className="flex">
+                                <div className="flex items-start gap-2">
                                     <span className="text-sm uppercase text-black-900">Date de 1ère Mise en Circ.:</span>
                                     <span className="text-sm font-semibold text-black">{formData.dateImmatriculation || '2022'}</span>
                                 </div>
-                                <div className="flex">
+                                <div className="flex items-start gap-2">
                                     <span className="text-sm uppercase text-black-900">Usage:</span>
                                     <span className="text-sm font-semibold text-black">{formData.usage}</span>
                                 </div>
-                                <div className="flex">
+                                <div className="flex items-start gap-2">
                                     <span className="text-sm uppercase text-black-900">N° Plaque:</span>
                                     <span className="text-sm font-semibold text-black">{formData.numeroPlaque || '0830AE22'}</span>
                                 </div>
